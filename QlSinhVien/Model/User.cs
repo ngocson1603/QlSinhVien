66ed08a1ef3ea7bcc754cf651494bf12344c7022
ngocson1603 <100844717+ngocson1603@users.noreply.github.com>
2023-10-26 22:36:18 +0700

    push code

---
+++ QlSinhVien/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using QlSinhVien.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QlSinhVien.Models
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+        [Required]
+        public string Role { get; set; } = null!;
+    }
+}
---
+++ QlSinhVien/ViewModels/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QlSinhVien.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Password { get; set; }
+    }
+}
